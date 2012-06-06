b69da650eb557fc00f5c65b1369ea451cb9917ac
Dan Little <dan.little@excensus.com>
2012-06-06 15:19:28 -0500

    Woohoo, setup.py works!

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pgSchemer',
+	version='0.0.1',
+	description='PostgreSQL Schema Management',
+	author='Dan Little',
+	author_email='danlittle@yahoo.com',
+	url='http://github.com/theduckylittle/pgschemer/',
+	packages=['pgschemer']
+)
